ec88a645da2d99edc802a60f5ed38707189b073a
Mateus Walkasther <mateuswalkasther@gmail.com>
2026-01-19 22:46:50 -0300

    Update exe067.py

--- Exercicios/exe067.py
+++ Exercicios/exe067.py
@@ -6,12 +6,10 @@ print('-='*10)
 
 n = 0
 while True:
-    c = 0
     n = int(input('\033[mDigite um número para ver a tabuada: '))
     if n < 0:
         break
-    while c <= 10:
-        print(f'\033[33m{n:<5}\033[mX\033[33m{c:^10}\033[m=\033[32m{c*n:>5}')
-        c += 1
-        print('-' * 25)
+    for c in range(0, 11):
+        print(f'\033[33m{n:<5}\033[97mX\033[33m{c:^10}\033[97m=\033[32m{c*n:>5}')
+    print('-' * 25)
 print('Por enquanto é só, volte logo!')
