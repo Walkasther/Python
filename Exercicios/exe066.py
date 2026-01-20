f3d8e56f678aff5c7cc6c8a5de904fc9f84d5b9d
Mateus Walkasther <mateuswalkasther@gmail.com>
2026-01-19 22:31:37 -0300

    Update exe066.py

--- Exercicios/exe066.py
+++ Exercicios/exe066.py
@@ -2,7 +2,7 @@
 #999, que é a condição de parada. No final, mostre quantos números foram digitados e qual foi a soma entre eles(Desconsiderando o flag).
 
 
-n = s = c = 0
+s = c = 0
 print(f'{'\033[32mSomador de números\033[m':-^50}')
 while True:
     n = int(input('Digite um número inteiro: '))
@@ -10,4 +10,4 @@ while True:
         break
     s += n
     c +=1
-print(f'\033[34mForam digitados {c} números, e a soma entre eles é {s}')
+print(f'\033[30;107mForam digitados {c} números, e a soma entre eles é {s}\033[m')
