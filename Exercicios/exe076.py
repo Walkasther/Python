7c38869de10d6f5d22b0945b470acd531e7fb1ac
Mateus Walkasther <mateuswalkasther@gmail.com>
2026-01-22 18:34:00 -0300

    Update exe076.py

--- Exercicios/exe076.py
+++ Exercicios/exe076.py
@@ -1,4 +1,7 @@
 #Crie um programa que tenha uma tupla única com nomes de produtos e seus respectivos preços, na sequência.
 #No final, mostre uma listagem de preços, organizando os dados em forma tabular.
 
-tupla_preço = 
+tupla_preço = 'Arroz',13.99, 'Feijão', 6.99, 'Açai', 5, 'poupa', 2.5, 'carne de sol',19.99
+
+for produto in tupla_preço:
+    ()
