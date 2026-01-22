e419132e157857bf8c51a942d7da8a07158fa5e2
Mateus Walkasther <mateuswalkasther@gmail.com>
2026-01-22 17:45:42 -0300

    update 74, 75

--- Exercicios/exe075.py
+++ Exercicios/exe075.py
@@ -2,3 +2,14 @@
 #A) Quantas vezes apareceu o valor 9.
 #B) Em que posição foi digitado o primeiro valor 3.
 #C) Quais foram os números pares.
+from itertools import count
+
+tupla_a = int(input('Valor1:')), int(input('Valor2:')), int(input('Valor3:')), int(input('Valor4:'))
+aparece9 = 0
+
+print(f'O número 9 apareceu {tupla_a.count(9)} vezes.')
+print(f'O número 3 aparece pela primeira vez na posição: {tupla_a.index(3)}')
+print(f'Os números pares digitados foram: ',end='')
+for i,c in enumerate(tupla_a):
+    if c % 2 == 0:
+        print(c, end=' ')
