a718d0ab214f78b4d7ff5f86b1b01745dc437ac9
Mateus Walkasther <mateuswalkasther@gmail.com>
2026-01-22 18:33:34 -0300

    update 75

--- Exercicios/exe075.py
+++ Exercicios/exe075.py
@@ -5,11 +5,16 @@
 from itertools import count
 
 tupla_a = int(input('Valor1:')), int(input('Valor2:')), int(input('Valor3:')), int(input('Valor4:'))
-aparece9 = 0
-
+p = 0
 print(f'O número 9 apareceu {tupla_a.count(9)} vezes.')
-print(f'O número 3 aparece pela primeira vez na posição: {tupla_a.index(3)}')
-print(f'Os números pares digitados foram: ',end='')
-for i,c in enumerate(tupla_a):
+if tupla_a.count(3) > 0: print(f'O número 3 aparece pela primeira vez na {tupla_a.index(3)+1}° posição.')
+else: print('O valor 3 não foi digitado em nenhuma posição')
+for c in tupla_a:
     if c % 2 == 0:
-        print(c, end=' ')
+        p = 1
+
+if p == 1:
+    print(f'Os números pares digitados foram: ', end='')
+    for c in tupla_a:
+        if c % 2 == 0: print(c, end=' ')
+else: print('Não foram digitados valores pares')
