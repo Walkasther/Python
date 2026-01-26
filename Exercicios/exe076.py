144b1523747c7311572aef46625ac289b035c362
Mateus Walkasther <mateuswalkasther@gmail.com>
2026-01-26 13:35:50 -0300

    update 76 ,77

--- Exercicios/exe076.py
+++ Exercicios/exe076.py
@@ -1,7 +1,9 @@
 #Crie um programa que tenha uma tupla única com nomes de produtos e seus respectivos preços, na sequência.
 #No final, mostre uma listagem de preços, organizando os dados em forma tabular.
 
-tupla_preço = 'Arroz',13.99, 'Feijão', 6.99, 'Açai', 5, 'poupa', 2.5, 'carne de sol',19.99
+tupla_preço = 'Arroz',13.99, 'Feijão', 6.99, 'Açai', 5, 'Poupa', 2.5, 'Carne de sol',19.99
 
-for produto in tupla_preço:
-    ()
+print(f'{'-'*50}\n{'Supermercado Guanabara':^50}\n{'-'* 50}')
+for c,produto in enumerate(tupla_preço):
+    if c % 2 == 0: print(f'{produto:.<40}',end='')
+    else: print(f'R$  {produto:.2f}')
--- Exercicios/exe077.py
+++ Exercicios/exe077.py
@@ -1,2 +1,11 @@
 #Crie um programa que tenha uma tupla com várias palavras(não usar acentos). Depois disso, você deve mostrar,
 # para cada palavra, quais são as vogais.
+
+tupla_vogal = 'aula', 'python', 'codigo', 'variavel', 'lista', 'tupla', 'loop', 'condicao', 'funcao', 'classe', 'objeto', 'metodo', 'arquivo', 'dados', 'numero', 'texto', 'entrada', 'saida', 'logica', 'programa'
+tupla2 = 1,2,3,4,5,6,7,89,9,10
+for c, palavra in enumerate(tupla_vogal):
+        print(f'Na palavra {palavra.upper()} temos: ',end='')
+        for i in range(0,len(palavra)):
+            if palavra[i].lower() in 'aeiou':
+                print(palavra[i], end=' ')
+        print()
