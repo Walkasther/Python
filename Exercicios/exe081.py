8b1b06c0522182d61b37eea165e68eef433340a3
Mateus Walkasther <mateuswalkasther@gmail.com>
2026-02-04 12:30:17 -0300

    update 81, 82

--- Exercicios/exe081.py
+++ Exercicios/exe081.py
@@ -4,3 +4,20 @@
 #B) A lista de valores, ordenada de forma decrescente.
 #C) Se o valor 5 foi digitado e está ou não na lista.
 
+lista_valores = []
+cont = 1
+while True:
+    lista_valores.append(int(input(f'Digite o {cont}° valor: ')))
+    cont += 1
+    continuar = input('Quer continuar? [S/N]: ').strip().upper()
+    if continuar == 'N':
+        break
+    print('-' * 30)
+lista_valores.sort(reverse=True)
+print('-=' * 15)
+print(f'Foram digitados {len(lista_valores)} números.')
+print(f'Os valores digitados foram {lista_valores}')
+if 5 in lista_valores:
+    print(f'O número 5 faz parte da lista e está na posição {lista_valores.index(5)}!')
+else:
+    print('O valor 5 não foi encontrado na lista!')
--- Exercicios/exe082.py
+++ Exercicios/exe082.py
@@ -1,3 +1,22 @@
 #Crie um programa que vai ler vários números e colocar em uma lista. Depois disso, crie duas listas extras que vão
 #conter apenas os valores pares e os valores impares digitados, respectivamente.
 #Ao final, mostre o conteúdo das três listas geradas.
+
+lista_valores = []
+lista_pares = []
+lista_impares = []
+while True:
+    lista_valores.append(int(input('Digite um número: ')))
+    cont = int(input('Quer continuar? [1]SIM [2]NÃO: '))
+    if cont == 2:
+        break
+
+for c in lista_valores:
+    if c % 2 == 0:
+        lista_pares.append(c)
+    else:
+        lista_impares.append(c)
+print('-=' * 25)
+print(f'\033[33mA lista completa é: {lista_valores}')
+print(f'\033[31mA lista de pares é: {lista_pares}')
+print(f'\033[32mA lista de Impares é {lista_impares}')
