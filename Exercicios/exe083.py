c86d5b11af35c0b720f33e5b5c3632d06e73ce43
Mateus Walkasther <mateuswalkasther@gmail.com>
2026-02-05 22:59:59 -0300

    Update exe083.py

--- Exercicios/exe083.py
+++ Exercicios/exe083.py
@@ -1,8 +1,6 @@
 #Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. Seu aplicativo deverá analisar
 # se a expressão passada está com os parenteses abertos e fechados na ordem correta.
 
-
-
 expressao = input('Digite a expressão: ').strip()
 expressao = ''.join(expressao.split())
 lista_expressao = []
