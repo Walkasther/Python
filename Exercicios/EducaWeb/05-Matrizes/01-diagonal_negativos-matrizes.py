45344344f8ac8e2ae0ada93b5f22b7f3c6673d48
Mateus Walkasther <mateuswalkasther@gmail.com>
2026-02-24 22:26:28 -0300

    UPDATE EXES

--- Exercicios/EducaWeb/05-Matrizes/01-diagonal_negativos-matrizes.py
+++ Exercicios/EducaWeb/05-Matrizes/01-diagonal_negativos-matrizes.py
@@ -1,2 +1,17 @@
 # Fazer um programa para ler um número inteiro N (máximo 10) e uma matriz quadrada de ordem N contendo números inteiros.
 # Em seguida, mostrar a diagonal principal e a quantidade de valores negativos da matriz.
+
+n = int(input('Qual a ordem da matriz? '))
+quantidade_negativos = 0
+
+matriz = [[int(input(f'Elemento [{i},{j}]: ')) for j in range(n)] for i in range(n)]
+
+for i in range(n):
+    for j in range(n):
+        if matriz[i][j] < 0:
+            quantidade_negativos += 1
+        if i == j:
+            print(f'{matriz[i][j]}', end='\n' if i+1 == n and j+1 == n else ' ')
+
+print(f'QUANTIDADE DE NEGATIVOS = {quantidade_negativos}')
+
--- Exercicios/EducaWeb/05-Matrizes/02-soma_linhas-matrizes.py
+++ Exercicios/EducaWeb/05-Matrizes/02-soma_linhas-matrizes.py
@@ -1,3 +1,24 @@
 # Fazer um programa para ler dois números inteiros M e N (máximo 10). Em seguida, ler uma matriz de M linhas e N
 # colunas contendo números reais. Gerar um vetor de modo que cada elemento do vetor seja a soma dos elementos da
 # linha correspondente da matriz. Mostrar o vetor gerado.
+
+m = int(input('Qual a quantidade de linhas da matriz? '))
+n = int(input('Qual a quantidade de colunas da matriz? '))
+
+matriz = []
+
+for i in range(m):
+    matriz.append([])
+    print(f'Digite os elementos da {i+1}a. linha: ')
+    for j in range(n):
+        matriz[i].append(float(input()))
+
+vetor = []
+
+for i in range(m):
+    vetor.append(sum(matriz[i]))
+
+
+print('VETOR GERADO:')
+for resultado in vetor:
+    print(f'{resultado}')
