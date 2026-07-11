690c78fa2ecb79b81ee4cb73607259548612173b
Mateus Walkasther <mateuswalkasther@gmail.com>
2026-07-11 15:49:22 -0300

    Update maior_e_menor_na_lista.py

--- Exercicios/EducaWeb/07-Listas/maior_e_menor_na_lista.py
+++ Exercicios/EducaWeb/07-Listas/maior_e_menor_na_lista.py
@@ -11,10 +11,10 @@ print(f'Valores Digitados: {valores}')
 print(f'O maior valor da lista é {maior}', 'na posição' if valores.count(maior) == 1 else 'nas posições', end =' ')
 for posicao, valor in enumerate(valores):
     if valor == maior:
-        print(posicao, end=' ')
+        print(posicao, end='... ')
 
 print(f'\nO menor valor da lista é {menor}', 'na posição' if valores.count(menor) == 1 else 'nas posições', end=' ')
 for posicao, valor in enumerate(valores):
     if valor == menor:
-        print(posicao, end=' ')
+        print(posicao, end='... ')
 
