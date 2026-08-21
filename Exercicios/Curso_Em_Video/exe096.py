6499a63a96eca598cfe76aa6e5ab694db89c2b15
Mateus Walkasther <mateuswalkasther@gmail.com>
2026-08-21 15:54:39 -0300

    Update exe096.py

--- Exercicios/Curso_Em_Video/exe096.py
+++ Exercicios/Curso_Em_Video/exe096.py
@@ -1,3 +1,15 @@
 #Faça um programa que tenha uma função chamada area(), que receba as dimensões de um terreno retângular (largura e comprimento)
 #e mostre a área do terreno.
 
+def area(a,b):
+    total = a * b
+    print(f'A área de um terreno {a}x{b} é de {total}m²')
+
+
+print('Controle dos terrenos')
+print('-'*30)
+
+largura = float(input('Largura: '))
+comprimento = float(input('Comprimento: '))
+
+area(largura,comprimento)
