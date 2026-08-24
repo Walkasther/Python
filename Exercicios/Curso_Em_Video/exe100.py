49d1ab2ff373ac005e143fb95c3c889401c7e8f4
Mateus Walkasther <mateuswalkasther@gmail.com>
2026-08-24 01:56:54 -0300

    Update exe100.py

--- Exercicios/Curso_Em_Video/exe100.py
+++ Exercicios/Curso_Em_Video/exe100.py
@@ -2,3 +2,24 @@
 #função vai sortear 5 números e colocá-los dentro da lista e a segunda função vai mostrar a soma entre todos os valores
 #pares sorteados pela função anterior.
 
+from random import randint
+from time import sleep
+
+def sorteia(lista):
+    print('Sorteando 5 valores da lista:')
+    for i in range(5):
+        sleep(0.25)
+        lista.append(randint(1,10))
+        print(lista[-1], end=' ')
+    print('PRONTO!')
+
+def somaPar(valores):
+    soma = sum(valor for valor in valores if valor % 2 == 0)
+    print(f'Somando os valores pares de {valores}, temos {soma}')
+
+
+#Inicio do programa
+numeros = []
+
+sorteia(numeros)
+somaPar(numeros)
