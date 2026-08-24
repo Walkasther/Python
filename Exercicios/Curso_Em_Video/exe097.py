08f665890de2a2553e38eb2a405c1f4f7dbf6bc6
Mateus Walkasther <mateuswalkasther@gmail.com>
2026-08-23 23:43:07 -0300

    update exe97  and exe98

--- Exercicios/Curso_Em_Video/exe097.py
+++ Exercicios/Curso_Em_Video/exe097.py
@@ -1,2 +1,11 @@
 #Faça um programa que tenha uma função chamada escreva(), que receba um texto qualquer e mostre uma mensagem com tamanho adaptável.
 
+def escreva(msg):
+    linha = '~' * len(msg)
+    print(linha)
+    print(msg)
+    print(linha)
+
+escreva('Gustavo Guanabara')
+escreva('Curso de Python no Youtube')
+escreva('CeV')
--- Exercicios/Curso_Em_Video/exe098.py
+++ Exercicios/Curso_Em_Video/exe098.py
@@ -4,3 +4,31 @@
 #b) de 10 até 0, de 2 em 2
 #c) uma contagem personalizada.
 
+from time import sleep
+
+def contador(inicio, fim, passo):
+    if passo == 0:
+        passo = 1
+
+    if inicio > fim and passo > 0:
+        passo = -passo
+
+    print('-=' * 30)
+    print(f'Contagem de {inicio} até {fim} contando de {abs(passo)} em {abs(passo)}')
+
+    if inicio > fim:
+        fim -= 2
+    for j in range(inicio, fim+1, passo):
+        sleep(0.25)
+        print(j, end=' ')
+    print('FIM!')
+
+
+#Inicio do programa:
+contador(1, 10, 1)
+contador(10, 0, -2)
+
+print('-=' * 30)
+print('Agora é a sua vez de personalizar a contagem!')
+
+contador(int(input('Inicio: ')), int(input('Fim: ')), int(input('Passo: ')))
